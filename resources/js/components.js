f31734b88d50ae29b44321965b3e0c11aa047d7f
Slavko Lazic <lazic.slavko.91@gmail.com>
2020-08-01 22:12:20 +0200

    Finished register front logic

--- resources/js/components.js
+++ resources/js/components.js
@@ -1,5 +1,7 @@
 import Vue from "vue"
 
 import Login from './components/auth/Login.vue';
+import Register from './components/auth/Register.vue';
 
 Vue.component('login', Login);
+Vue.component('register', Register);
