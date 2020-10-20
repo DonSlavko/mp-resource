362d9866576682f9168b7d5d334dda40890d0e6e
Entoni Bencun <entoni.bencun@gmail.com>
2020-10-20 14:19:22 +0200

    Added pdf templates

---
+++ resources/views/pdf/invoice.blade.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aloha!</title>
+
+    <style type="text/css">
+        * {
+            font-family: Verdana, Arial, sans-serif;
+        }
+        table{
+            font-size: x-small;
+        }
+        tfoot tr td{
+            font-weight: bold;
+            font-size: x-small;
+        }
+
+        .gray {
+            background-color: lightgray
+        }
+    </style>
+
+</head>
+<body>
+
+<table width="100%">
+    <tr>
+        <td valign="top">{{--<img src="{{asset('images/meteor-logo.png')}}" alt="" width="150"/>--}}</td>
+
+        <td align="left">
+            <p>Medical Pharma Resource GmbH Marktplatz 1  40764  Langenfeld</p>
+            <p>
+                Musterapotheke <br>
+                Musterstrastraße 12 <br>
+                67891 Musterstadt
+            </p>
+            <pre>
+
+            </pre>
+        </td>
+
+        <td align="right">
+            <h3></h3>
+            <p>
+
+
+
+            </p>
+        </td>
+    </tr>
+
+</table>
+
+<table width="100%">
+    <tr>
+        <td>
+            <h1>Rechnung</h1>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <strong>Rechnungsnummer:</strong><br>
+            xxxxXXXXXxxx
+        </td>
+        <td>
+            <strong>Kundennummer:</strong><br>
+            xxxxxXXXxxxx
+        </td>
+        <td>
+            <strong>Auftragsnummer:</strong><br>
+            xxxxxXXXxxxx
+        </td>
+        <td>
+            <strong>Datum:</strong><br>
+            TT.MM.JJJJ
+        </td>
+    </tr>
+</table>
+
+<br/>
+
+<table width="100%">
+    <thead style="background-color: lightgray;">
+    <tr>
+        <th>Position</th>
+        <th>Anzahl</th>
+        <th>Preis</th>
+        <th>EinHeit(g/Kg)</th>
+        <th>Beschreibung</th>
+        <th>Steuer</th>
+        <th>Netto</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>Playstation IV - Black</td>
+        <td align="right">1</td>
+        <td align="right">1400.00</td>
+        <td align="right">1400.00</td>
+        <td>Test</td>
+        <td>Test</td>
+    </tr>
+    </tbody>
+
+    <tfoot>
+    <tr>
+        <td colspan="3"></td>
+        <td align="right">Zwischensumme Netto</td>
+        <td align="right">1635.00</td>
+    </tr>
+    <tr>
+        <td colspan="3"></td>
+        <td align="right">Umsatzsteuer 19%</td>
+        <td align="right">294.3</td>
+    </tr>
+    <tr>
+        <td colspan="3"></td>
+        <td align="right">Gesamtbetrag</td>
+        <td align="right" class="gray">$ 1929.3</td>
+    </tr>
+    </tfoot>
+</table>
+
+<table width="100%">
+    <tr>
+        <td align="left">
+            <p>
+                Sollten Sie den Gesamtbetrag nicht per Vorkasse oder Sofortüberweisung überweisen haben, dann
+                werden wir es innerhalb von 14 Tagen über den von Ihnen erteilten SEPA-Einzug einfordern.
+            </p>
+            <p>Für weitere Fragen können Sie sich gerne mit uns in Verbindung setzen.
+            </p>
+            <p>Mit freundlichen Grüßen</p>
+            <p>Ihr MPR-Team</p>
+        </td>
+    </tr>
+</table>
+
+<table width="100%">
+    <tr>
+        <td>Medical Pharma Resource GmbH <br>
+            Markplatz 1<br>
+            40746 Largenfeld <br>
+            Deutschland
+        </td>
+        <td>CEO: Eng in Ates <br>
+            St-Nr: 230/5712/3624 <br>
+            USt.-Id-Nr: DE323439826 <br>
+            Handelsregister: HRB 88695 <br>
+            AG Düsseldorf
+        </td>
+        <td>Telefon: 02173/ 940 94 91 <br>
+            Telefax: 02173/ 940 94 99 <br>
+            E-Mail: info@mp-resource.shop <br>
+            Web: mp-resource.shop
+        </td>
+        <td>
+            Bank: Commerzbank Köln <br>
+            IBAN: DE81 3704 0044 0505 3525 00 <br>
+            BIC: COBADEFFXXX
+        </td>
+    </tr>
+
+</table>
+</body>
+</html>
---
+++ resources/views/pdf/order.blade.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aloha!</title>
+
+    <style type="text/css">
+        * {
+            font-family: Verdana, Arial, sans-serif;
+        }
+        table{
+            font-size: x-small;
+        }
+        tfoot tr td{
+            font-weight: bold;
+            font-size: x-small;
+        }
+
+        .gray {
+            background-color: lightgray
+        }
+    </style>
+
+</head>
+<body>
+
+<table width="100%">
+    <tr>
+        <td valign="top">{{--<img src="{{asset('images/meteor-logo.png')}}" alt="" width="150"/>--}}</td>
+
+        <td align="left">
+            <p>Medical Pharma Resource GmbH Marktplatz 1  40764  Langenfeld</p>
+            <p>
+                Musterapotheke <br>
+                Musterstrastraße 12 <br>
+                67891 Musterstadt
+            </p>
+            <pre>
+
+            </pre>
+        </td>
+
+        <td align="right">
+            <h3></h3>
+            <p>
+                <strong>Kundennummer:</strong><br>
+                xxxxxXXXxxxx <br>
+                <strong>Auftragsnummer:</strong><br>
+                xxxxXXXXXxxx <br>
+                <strong>Datum:</strong><br>
+                TT.MM.JJJJ
+                <strong>Bezug:</strong><br>
+                <strong>Vobestellungsummer:</strong><br>
+                xxxxXXXXXxxx <br>
+
+            </p>
+        </td>
+    </tr>
+
+</table>
+
+<table width="100%">
+    <tr>
+        <td>
+            <h1>Auftragsbestätigung</h1>
+            <p>
+                Sehr geehrte Damen und Herren,<br>
+                vielen Dank fur ihre verbindliche Vorbestellung und ihr Verstrauen. Wir freuen uns uber
+                ihr Interesse an unseren Produkten. Sie haben folgende Produkte bei uns vorbestellt:
+
+            </p>
+        </td>
+    </tr>
+
+</table>
+
+<br/>
+
+<table width="100%">
+    <thead style="background-color: lightgray;">
+    <tr>
+        <th>Position</th>
+        <th>Anzahl</th>
+        <th>Preis</th>
+        <th>EinHeit(g/Kg)</th>
+        <th>Beschreibung</th>
+        <th>Steuer</th>
+        <th>Netto</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>Playstation IV - Black</td>
+        <td align="right">1</td>
+        <td align="right">1400.00</td>
+        <td align="right">1400.00</td>
+        <td>Test</td>
+        <td>Test</td>
+    </tr>
+    </tbody>
+
+    <tfoot>
+    <tr>
+        <td colspan="3"></td>
+        <td align="right">Zwischensumme Netto</td>
+        <td align="right">1635.00</td>
+    </tr>
+    <tr>
+        <td colspan="3"></td>
+        <td align="right">Umsatzsteuer 19%</td>
+        <td align="right">294.3</td>
+    </tr>
+    <tr>
+        <td colspan="3"></td>
+        <td align="right">Gesamtbetrag</td>
+        <td align="right" class="gray">$ 1929.3</td>
+    </tr>
+    </tfoot>
+</table>
+
+<table width="100%">
+    <tr>
+        <td align="left">
+            <p>
+
+            </p>
+            <p>Ihr Rechnung wird parallel erstellt und Ihnen zeitnah versendet, Solten Sie weitere Fragen haben, dürfen Sie uns gerne kontaktieren.
+            </p>
+            <p>Mit freundlichen Grüßen</p>
+            <p>Ihr MPR-Team</p>
+        </td>
+    </tr>
+</table>
+
+<table width="100%">
+    <tr>
+        <td>Medical Pharma Resource GmbH <br>
+            Markplatz 1<br>
+            40746 Largenfeld <br>
+            Deutschland
+        </td>
+        <td>CEO: Eng in Ates <br>
+            St-Nr: 230/5712/3624 <br>
+            USt.-Id-Nr: DE323439826 <br>
+            Handelsregister: HRB 88695 <br>
+            AG Düsseldorf
+        </td>
+        <td>Telefon: 02173/ 940 94 91 <br>
+            Telefax: 02173/ 940 94 99 <br>
+            E-Mail: info@mp-resource.shop <br>
+            Web: mp-resource.shop
+        </td>
+        <td>
+            Bank: Commerzbank Köln <br>
+            IBAN: DE81 3704 0044 0505 3525 00 <br>
+            BIC: COBADEFFXXX
+        </td>
+    </tr>
+
+</table>
+</body>
+</html>
