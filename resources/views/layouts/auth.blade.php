00ed5d2d5747108dd42e0432986f27a27462620d
Slavko Lazic <lazic.slavko.91@gmail.com>
2020-10-23 21:03:27 +0200

    Small changes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,7 +7,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'MP Resource') }}</title>
+
+    <link rel="icon" href="/images/cropped-mpr_rund_neu-32x32.png" sizes="32x32">
+    <link rel="icon" href="/images/cropped-mpr_rund_neu-192x192.png" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="/images/cropped-mpr_rund_neu-180x180.png">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
